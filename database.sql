d39f00f4a55f60b4b0ca9b925c3c17dcc2c8088f
miti <mrmax.meti90@gmail.com>
2025-01-03 02:43:52 -0800

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE messenger;
+
+USE messenger;
+
+CREATE TABLE messages (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    sender VARCHAR(50) NOT NULL,
+    text TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
